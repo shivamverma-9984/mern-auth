5cb9e8e0892e61e0031a3b8438202c678c9db8b5
Shivam kumar <shivam.ss.kv@gmail.com>
2025-02-09 01:37:52 +0530

    ui updated

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const path=require('path');
 require('dotenv').config();
+const connection=require('./Database/connetion')
+const User=require('./model/userSchema')
 
 const app = express();
 app.use(express.json());
@@ -16,20 +17,19 @@ app.use(cors({
 }));
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+// mongoose.connect(process.env.MONGO_URI)
+// .then(() => console.log('MongoDB connected'))
+// .catch(err => console.log(err));
+connection()
+
 
 // User Schema
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+// const userSchema = new mongoose.Schema({
+//   username: { type: String, required: true, unique: true },
+//   password: { type: String, required: true },
+// });
 
-const User = mongoose.model('User', userSchema);
+// const User = mongoose.model('User', userSchema);
 
 // Register Route
 app.post('/register', async (req, res) => {
@@ -64,7 +64,7 @@ app.post('/login', async (req, res) => {
 
 app.use(express.static(path.join(_dirname,'dist')))
 app.get('*',(req,res)=>{
-  res.sendFile(path.join(_dirname,'../client/dist','index.html'))
+  res.sendFile(path.join(_dirname,'dist','index.html'))
 })
 
 // Start the server
